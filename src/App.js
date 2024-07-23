ea7d71eed447243d1305c1ee031a14d3e29bac42
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2024-07-23 13:27:00 +0300

    use state variable with useState

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,35 +7,25 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 1
+  const [step, setStep] = useState(1)
   const buttonStyle = {
     backgroundColor: '#7950f2',
     color: '#ffffff',
   }
 
   function handlePrevious() {
-    alert('Previous')
-    // if (step < 3) {
-    //   setStep(step + 1);
-    // } else {
-    //   alert('You have completed all steps');
-    // }
+    if (step > 1) setStep(step - 1);
   }
   function handleNext() {
-    alert('Next')
-    // if (step < 3) {
-    //   setStep(step + 1);
-    // } else {
-    //   alert('You have completed all steps');
-    // }
+    if (step < 3) setStep(step + 1);
   }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 && "active"}`}>1</div>
-        <div className={`${step >= 2 && "active"}`}>2</div>
-        <div className={`${step >= 3 && "active"}`}>3</div>
+        <div className={step >= 1 && "active"}>1</div>
+        <div className={step >= 2 && "active"}>2</div>
+        <div className={step >= 3 && "active"}>3</div>
       </div>
       <p className="message">Step {step} {messages[step - 1]}</p>
       <div className="buttons">
